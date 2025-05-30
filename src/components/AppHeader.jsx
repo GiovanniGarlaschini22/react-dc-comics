8c262f3c9d217cc43aa41080d94c503f812f610c
GiovanniGarlaschini22 <giovannigarlaschini.boolean@gmail.com>
2025-05-30 18:03:00 +0200

    Feat: header creata: - AppHeader.jsx fatto - AppHeader.module.css creato e gestito css da qui anzichè da Index.css - FavIcon aggiunta

--- src/components/AppHeader.jsx
+++ src/components/AppHeader.jsx
@@ -1,5 +1,46 @@
+// IMPORT //
+import style from "./AppHeader.module.css";
+
 function AppHeader() {
-    return <header>header</header>;
+    return (
+        <header className={style.header}>
+            <img src="/dc-logo.png" alt="logo-header-dc" />
+            <nav>
+                <ul>
+                    <li>
+                        <a href="">characters</a>
+                    </li>
+                    <li>
+                        <a href="">comics</a>
+                    </li>
+                    <li>
+                        <a href="">movies</a>
+                    </li>
+                    <li>
+                        <a href="">tv</a>
+                    </li>
+                    <li>
+                        <a href="">games</a>
+                    </li>
+                    <li>
+                        <a href="">collectibles</a>
+                    </li>
+                    <li>
+                        <a href="">videos</a>
+                    </li>
+                    <li>
+                        <a href="">fans</a>
+                    </li>
+                    <li>
+                        <a href="">news</a>
+                    </li>
+                    <li>
+                        <a href="">shop</a>
+                    </li>
+                </ul>
+            </nav>
+        </header>
+    );
 }
 
 export default AppHeader;
